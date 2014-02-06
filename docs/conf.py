085c02e10476adabed5f5e234bb93ce18747cbed
Simon de Haan <simon@praekeltfoundation.org>
2014-02-06 17:21:18 +0200

    fix docs titles

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Praekelt Ways of Working'
+project = u'Praekelt Patterns'
 copyright = u'2013, Praekelt Foundation devs and individual contributors'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -167,7 +167,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'PraekeltWaysofWorkingdoc'
+htmlhelp_basename = 'PraekeltPatterns'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -186,7 +186,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'PraekeltWaysofWorking.tex', u'Praekelt Ways of Working Documentation',
+  ('index', 'PraekeltPatterns.tex', u'Praekelt Patterns',
    u'Praekelt Foundation devs and individual contributors', 'manual'),
 ]
 
@@ -216,7 +216,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'praekeltwaysofworking', u'Praekelt Ways of Working Documentation',
+    ('index', 'praekeltpatterns', u'Praekelt Patterns',
      [u'Praekelt Foundation devs and individual contributors'], 1)
 ]
 
@@ -230,8 +230,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'PraekeltWaysofWorking', u'Praekelt Ways of Working Documentation',
-   u'Praekelt Foundation devs and individual contributors', 'PraekeltWaysofWorking', 'One line description of project.',
+  ('index', 'PraekeltPatterns', u'Praekelt Patterns',
+   u'Praekelt Foundation devs and individual contributors', 'PraekeltPatterns', 'One line description of project.',
    'Miscellaneous'),
 ]
 
